abc527b0048c96d799cb81820350ae9624fdc3b1
MoHoDu <imvagueness@gmail.com>
2025-06-01 22:46:13 +0900

    [add] Add the Combination info class

--- Assets/Scripts/ManagerSystem/InGame/CombinationManager.cs
+++ Assets/Scripts/ManagerSystem/InGame/CombinationManager.cs
@@ -4,16 +4,28 @@ using InGame.Combination;
 
 namespace ManagerSystem.InGame
 {
+    public class CollectionInfo
+    {
+        // 오더 정보 
+        public List<RecipeData> OrderList = new();
+        
+        // 배치된 재료 정보 
+        public List<IngredientData> PlacedIngredients = new();
+        
+        // 수집한 레시피, 재료 목록
+        public List<RecipeData> CollectedRecipes = new();
+        public List<IngredientData> CollectedIngredients = new();
+    }
+    
     public class CombinationManager : BaseManager
     {
         // 컨트롤러
         public OrderSystem Order { get; private set; } = new();
         public IngredientPlacer IngredientPlacer { get; private set; } = new();
         
-        // 수집한 레시피, 재료 목록
-        public List<RecipeData> CollectedRecipes = new();
-        public List<IngredientData> CollectedIngredients = new();
-        
+        // 수집 정보 
+        public CollectionInfo Collection = new();
+         
         // DI
         private PrapManager _prapManager;
 
