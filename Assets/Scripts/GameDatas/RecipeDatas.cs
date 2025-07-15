f0a095cc1b200909ba008e41f86c68da0657e145
MoHoDu <imvagueness@gmail.com>
2025-07-15 17:34:08 +0900

    feat: Changes placing inner ingredients logic
    프리셋 이미지가 있다면 프리셋을 우선 적용하는 것으로 변경

--- Assets/Scripts/GameDatas/RecipeDatas.cs
+++ Assets/Scripts/GameDatas/RecipeDatas.cs
@@ -8,6 +8,7 @@ namespace GameDatas
     public class RecipeDatas : BaseData<RecipeData>
     {
         public Dictionary<string, RecipeData> Data { get; private set; } = new Dictionary<string, RecipeData>();
+        public Dictionary<long, RecipeData> DataByIngredientsMask { get; private set; } = new Dictionary<long, RecipeData>();
 
         public SortedList<float, List<RecipeData>> MinDistanceToRecipe { get; private set; } = new();
 
@@ -15,6 +16,8 @@ namespace GameDatas
         {
             Data.Clear();
             Data = inList.ToDictionary(x => x.id);
+            DataByIngredientsMask.Clear();
+            DataByIngredientsMask = inList.ToDictionary(x => GetIngredientsMask(x), x => x);
 
             foreach (var item in inList)
             {
@@ -29,11 +32,21 @@ namespace GameDatas
             }
         }
 
+        private long GetIngredientsMask(RecipeData recipe)
+        {
+            long mask = 0;
+            foreach (var ingredient in recipe.requiredIngredients)
+            {
+                mask |= (1L << (int)ingredient);
+            }
+            return mask;
+        }
+
         public RecipeData Get(string id)
         {
             return Data.ContainsKey(id) ? Data[id] : null;
         }
-        
+
         /// <summary>
         /// 이동 거리에 추가된 최신 레시피 리스트 반환
         /// </summary>
@@ -43,7 +56,7 @@ namespace GameDatas
         {
             // 바이너리 서치로 인덱스 찾기
             int index = MinDistanceToRecipe.Keys.ToList().BinarySearch(distance);
-            
+
             // 만약 0 이하면, 값이 없어 새롭게 추가할 때의 인덱스를 가리킴
             // ex) -2 (값은 없으나 들어간다면, 2번째 인덱스에 있게 될 것
             // 즉, ~로 비트 반전 연산을 하고, 그 이전의 값을 얻기 위해 -1
@@ -53,6 +66,33 @@ namespace GameDatas
                 return MinDistanceToRecipe.Values[index];
             else
                 return null;
+        }
+
+        public RecipeData GetRecipeFromIngredients(params IngredientData[] ingredients)
+        {
+            if (ingredients == null || ingredients.Length == 0)
+                return null;
+
+            // 재료의 비트 마스크 생성
+            long ingredientsMask = 0;
+            foreach (var ingredient in ingredients)
+            {
+                if (ingredient != null)
+                {
+                    ingredientsMask |= 1L << (int)ingredient.groupId;
+                }
+            }
+
+            // 해당 비트 마스크에 맞는 레시피 찾기
+            if (DataByIngredientsMask.TryGetValue(ingredientsMask, out var recipe))
+            {
+                return recipe;
+            }
+            else
+            {
+                // 해당 비트 마스크에 맞는 레시피가 없으면 null 반환
+                return null;
+            }
         }
     }
 }
--- Assets/Scripts/UIs/IngredientsInKimbapUI.cs
+++ Assets/Scripts/UIs/IngredientsInKimbapUI.cs
@@ -3,19 +3,24 @@ using System.Linq;
 using ManagerSystem;
 using UIs.Base;
 using UnityEngine;
+using Attributes;
+using UnityEngine.UI;
 
 namespace UIs
 {
     public class IngredientsInKimbapUI : BindUI
     {
+        [Bind("Preset")] private SpriteRenderer _presetSpriteRenderer;
+        [Bind("Preset")] private Image _presetImage;
+
         [Tooltip("내부 위치 설정을 담고 있는 ScriptableObject")]
         public InnerPlacePositionSO Settings;
-        
+
         private List<CollectedIngredient> _ingredients = new();
         private int _ingredientsMaxCount;
 
         private RectTransform _rect = null;
-        
+
 #if UNITY_EDITOR
         private void OnValidate()
         {
@@ -26,7 +31,7 @@ namespace UIs
             }
         }
 #endif
-        
+
         protected override void Initialize()
         {
             base.Initialize();
@@ -51,30 +56,122 @@ namespace UIs
             {
                 return false;
             }
-            
+
             _ingredients.Add(ingredient);
             UpdatePosition();
 
             return true;
         }
 
-        private void UpdatePosition()
+        private bool SetPresetOnCanvas(RecipeData recipeData)
         {
-            int currentCount = _ingredients.Count;
-            InnerPosition? positionInfo = Settings.GetPositionInfo(currentCount); 
-            if (positionInfo.HasValue)
+            if (_presetImage == null)
             {
+                Debug.LogWarning("Preset Image is not assigned.", this);
+                Initialize();
+            }
+
+            // 프리셋을 가지고 와서 적용 
+            _presetImage.color = Color.white;
+            SpriteRenderer sr = null;
+            var obj = Resources.Load<GameObject>($"Prefabs/Ingredients/Inner/Presets/{recipeData.groupId}");
+            obj?.TryGetComponent(out sr);
+            if (sr != null)
+            {
+                _presetImage.sprite = sr.sprite;
+                int currentCount = _ingredients.Count;
                 for (int i = 0; i < currentCount; i++)
                 {
-                    Vector3 ingredientPosition = positionInfo.Value.placePositions[i];
-                    if (_rect)
+                    if (_rect && _ingredients[i].TryGetComponent(out Image target))
                     {
-                        if (_ingredients[i].TryGetComponent(out RectTransform target))
-                        {
-                            target.anchoredPosition = ingredientPosition;
-                        }
+                        target.color = Color.clear;
                     }
-                    else _ingredients[i].transform.localPosition = ingredientPosition;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool SetPreset(RecipeData recipeData)
+        {
+            if (_presetSpriteRenderer == null)
+            {
+                Debug.LogWarning("Preset Image is not assigned.", this);
+                Initialize();
+            }
+
+            // 프리셋을 가지고 와서 적용 
+            _presetSpriteRenderer.color = Color.white;
+            SpriteRenderer sr = null;
+            var obj = Resources.Load<GameObject>($"Prefabs/Ingredients/Inner/Presets/{recipeData.groupId}");
+            obj?.TryGetComponent(out sr);
+            if (sr != null)
+            {
+                _presetSpriteRenderer.sprite = sr.sprite;
+                int currentCount = _ingredients.Count;
+                for (int i = 0; i < currentCount; i++)
+                {
+                    _ingredients[i].gameObject?.SetActive(false);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdatePosition()
+        {
+            // 존재하는 조합이 있는지 확인
+            IngredientData[] ingredientDatas = _ingredients.Select(i => i.Data).ToArray();
+            RecipeData recipe = DataContainer.RecipeDatas.GetRecipeFromIngredients(ingredientDatas);
+            bool setPresetSprite = false;
+            if (recipe != null)
+            {
+                if (_presetSpriteRenderer != null)
+                {
+                    setPresetSprite = SetPreset(recipe);
+                }
+                else if (_presetImage != null)
+                {
+                    setPresetSprite = SetPresetOnCanvas(recipe);
+                }
+            }
+
+            if (recipe == null || !setPresetSprite)
+            {
+                if (_presetSpriteRenderer)
+                {
+                    _presetSpriteRenderer.sprite = null;
+                    _presetSpriteRenderer.color = Color.clear;
+                }
+                else if (_presetImage)
+                {
+                    _presetImage.sprite = null;
+                    _presetImage.color = Color.clear;
+                }
+
+                int currentCount = _ingredients.Count;
+                InnerPosition? positionInfo = Settings.GetPositionInfo(currentCount);
+                if (positionInfo.HasValue)
+                {
+                    for (int i = 0; i < currentCount; i++)
+                    {
+                        Vector3 ingredientPosition = positionInfo.Value.placePositions[i];
+
+                        _ingredients[i].gameObject?.SetActive(true);
+
+                        if (_rect)
+                        {
+                            if (_ingredients[i].TryGetComponent(out RectTransform target))
+                            {
+                                target.anchoredPosition = ingredientPosition;
+                            }
+                        }
+                        else _ingredients[i].transform.localPosition = ingredientPosition;
+                    }
                 }
             }
         }
